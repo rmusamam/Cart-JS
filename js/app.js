e1e4286c637eeb31e3fda921538bde5837cac109
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-06-03 16:45:51 +0500

    working on cart adding pic

--- js/app.js
+++ js/app.js
@@ -1,23 +1,38 @@
-// show cart 
+// show cart
 
-(function(){
-const cartInfo = document.getElementById('cart-info');
+(function () {
+  const cartInfo = document.getElementById("cart-info");
 
-const cart = document.getElementById('cart');
+  const cart = document.getElementById("cart");
 
-cartInfo.addEventListener('click',function(){
-    cart.classList.toggle('show-cart');
-})
+  cartInfo.addEventListener("click", function () {
+    cart.classList.toggle("show-cart");
+  });
 })();
 
-//add items in to cart 
+//add items in to cart
 
-(function(){
+(function () {
+  const cartBtn = document.querySelectorAll(".store-item-icon");
 
+  cartBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+      //console.log(event.target.parentElement.classList)
+      if (event.target.parentElement.classList.contains("store-item-icon")) {
+        let fullPath = event.target.parentElement.previousElementSibling.src;
+        let pos = fullPath.indexOf("img") + 3;
+        //console.log(pos);
+        let partPath = fullPath.slice(pos);
+        
+        const item ={};
+        item.img=`img-cart${partPath}`;
 
+        //let name =  event.target.parentElement.parentElement.nextElemetSibling.children[0].children[0].textContent;
+        
+        // console.log(name);
+        
 
-
-
-    
+      }
+    });
+  });
 })();
-
