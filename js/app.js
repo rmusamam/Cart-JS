e43fede6962fafa535f84b83bec4ab2b6cdcc895
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-06-03 17:44:12 +0500

    Cart info get

--- js/app.js
+++ js/app.js
@@ -27,11 +27,36 @@
         const item ={};
         item.img=`img-cart${partPath}`;
 
-        //let name =  event.target.parentElement.parentElement.nextElemetSibling.children[0].children[0].textContent;
-        
-        // console.log(name);
         
 
+        let name =  event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+        
+        let price  =  event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+        
+        let finalPrice = price.slice(1).trim();
+        
+        item.name=name;
+        item.price=finalPrice;
+
+       // console.log(item);
+
+       const cartItem = document.createElement('div');
+       cartItem.classList('cart-Item','d-flex','justify-content-between','text-capitalize','my-3');
+
+       cartItem.innerHTML=` 
+       <img src="${item.img}" class="img-fluid 
+       rounded-circle" id="item-img" alt="">
+       <div class="item-text">
+
+         <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+         <span>$</span>
+         <span id="cart-item-price" class="cart-item-price" 
+         class="mb-0">${item.price}</span>
+       </div>
+       <a href="#" id='cart-item-remove' class="cart-item-remove">
+         <i class="fas fa-trash"></i>
+       </a>
+     </div>`;
       }
     });
   });
