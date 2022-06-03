4cb40ad775a8a262fa98748313d779fc6abe3d5f
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-06-03 14:44:19 +0500

    show cart function added

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// show cart 
+
+(function(){
+const cartInfo = document.getElementById('cart-info');
+
+const cart = document.getElementById('cart');
+
+cartInfo.addEventListener('click',function(){
+    cart.classList.toggle('show-cart');
+})
+})();
+
+//add items in to cart 
+
+(function(){
+
+
+
+
+
+    
+})();
+
