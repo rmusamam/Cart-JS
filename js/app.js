05f57e8c8594a848757efe52d7859c60a2b81d01
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-06-04 15:40:19 +0500

    all project done in JS

--- js/app.js
+++ js/app.js
@@ -23,27 +23,35 @@
         let pos = fullPath.indexOf("img") + 3;
         //console.log(pos);
         let partPath = fullPath.slice(pos);
-        
-        const item ={};
-        item.img=`img-cart${partPath}`;
 
-        
+        const item = {};
+        item.img = `img-cart${partPath}`;
 
-        let name =  event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
-        
-        let price  =  event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
-        
+        let name =
+          event.target.parentElement.parentElement.nextElementSibling
+            .children[0].children[0].textContent;
+
+        let price =
+          event.target.parentElement.parentElement.nextElementSibling
+            .children[0].children[1].textContent;
+
         let finalPrice = price.slice(1).trim();
-        
-        item.name=name;
-        item.price=finalPrice;
 
-       // console.log(item);
+        item.name = name;
+        item.price = finalPrice;
 
-       const cartItem = document.createElement('div');
-       cartItem.classList.add('cart-Item','d-flex','justify-content-between','text-capitalize','my-3');
+        // console.log(item);
 
-       cartItem.innerHTML=` 
+        const cartItem = document.createElement("div");
+        cartItem.classList.add(
+          "cart-Item",
+          "d-flex",
+          "justify-content-between",
+          "text-capitalize",
+          "my-3"
+        );
+
+        cartItem.innerHTML = ` 
        <img src="${item.img}" class="img-fluid 
        rounded-circle" id="item-img" alt="">
        <div class="item-text">
@@ -58,41 +66,60 @@
        </a>
      </div>`;
 
-     // select Cart 
+        // select Cart
 
-     const cart = document.getElementById('cart');
-     const total = document.querySelector('.cart-total-container')
-      
-     cart.insertBefore(cartItem,total);
-     alert('Item added to cart');
-    
-    showTotals();
-    }
+        const cart = document.getElementById("cart");
+        const total = document.querySelector(".cart-total-container");
+
+        cart.insertBefore(cartItem, total);
+        alert("Item added to cart");
+
+        showTotals();
+      }
     });
   });
 
-  //show total 
-  function showTotals(){
-    const total=[];
-    const items = document.querySelectorAll('.cart-item-price');
-    //debugger;
-    items.forEach(function(item){
-        total.push(parseFloat(item.textContent));
-
+  //show total
+  function showTotals() {
+    const total = [];
+    const items = document.querySelectorAll(".cart-item-price");
+    items.forEach(function (item) {
+      total.push(parseFloat(item.textContent));
     });
 
-    const totalMoney= total.reduce(function(total,item){
-        total+=item;
-        return total;
-    },0);
+    const totalMoney = total.reduce(function (total, item) {
+      total += item;
+      return total;
+    }, 0);
     const finalMoney = totalMoney.toFixed(2);
     // console.log(finalMoney)
-    
-    document.getElementById('cart-total').textContent= finalMoney;
-    debugger;
-    document.querySelector('.item-total').textContent= finalMoney;
-    document.getElementById('.item-count').textContent= total.length;
 
-
+    document.getElementById("cart-total").textContent = finalMoney;
+    document.querySelector(".item-total").textContent = finalMoney;
+    document.getElementById("item-count").textContent = total.length;
   }
+})();
+
+// clear Cart
+
+(function () {
+  let clr = document.getElementById("clear-cart");
+
+  clr.addEventListener("click", function () {
+    document.querySelector(".item-total").textContent = 0;
+    document.querySelector("#cart-total").textContent = 0;
+    let clearCartItem = document.getElementsByClassName("cart-Item"); // HTMLCollection
+
+    Array.from(clearCartItem).forEach(function(el) {
+      el.remove()
+    })
+  });
+})();
+
+(function(){
+  let bin=document.getElementById('cart-item-remove')
+  // console.log(bin)
+  bin.addEventListener('click',function(event){
+   console.log(event.target.parentElement.parentElement) 
+  })
 })();
